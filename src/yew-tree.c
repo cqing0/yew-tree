c20f3e7284228cf8e3e6f50888c086013af98f39
cqing0 <boerbasher@protonmail.com>
2024-01-12 16:36:16 +0000

    Create path concat

--- src/yew-tree.c
+++ src/yew-tree.c
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <tchar.h>
+#include <string.h>
 #include "write_test_data.h"
 
 // Temp max path length
@@ -29,18 +30,21 @@ void listFilesInDir(TCHAR * path)
 int main()
 {
 
-	TCHAR buffer[MAX_PATH];
-	DWORD length = GetCurrentDirectory(MAX_PATH, buffer);
+	TCHAR runningFromPath[MAX_PATH];
+	DWORD length = GetCurrentDirectory(MAX_PATH, runningFromPath);
+	TCHAR relativePath[] = "\\test_data\\*.txt";
+
+	size_t totalSizeOfPath = strlen(runningFromPath) + strlen(relativePath) + 1;
+	
+	TCHAR pathJoined[totalSizeOfPath];
+
+	// Join the paths together
+	strcpy(pathJoined, runningFromPath);
+	strcat(pathJoined, relativePath);
 	
 	//printf("Trying to generate many text files...\n");
 	//createManyFiles();
 	
-	// Join the paths together
-	TCHAR * workingDir = buffer;
-	char testDataDir[] = "test_data";
-	//TODO Make this take in the relative path
-	char pathJoined[] = "C:\\work\\yew-tree\\test_data\\*.txt";
-
 	listFilesInDir(pathJoined);
 
 	return 0;
